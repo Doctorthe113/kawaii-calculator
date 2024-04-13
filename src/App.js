f9d431b0cad0ef0efd8ff8eedfa151aa2c38fae3
doctorthe113 <code.doc113@gmail.com>
2024-04-13 18:29:22 +0600

    added the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [output, setOutput] = useState("");
+  if (output.length > 11){
+    setOutput("Too big");
+    setTimeout(() => setOutput(""), 2000)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="calc">
+      <div className="display">
+        <p className="output">{output}</p>
+      </div>
+      <span className="name">Kawaii Calc :3</span>
+      <div className="keypad">
+        <button onClick={() => setOutput("")}>AC</button>
+        <button onClick={() => setOutput(output.slice(0, -1))}>DE</button>
+        <button onClick={() => setOutput(Math.sqrt(output).toPrecision(10))}>√</button>
+        <button onClick={() => setOutput((output * output).toString())}>^2</button>
+        <button onClick={() => setOutput(output + "7")}>7</button>
+        <button onClick={() => setOutput(output + "8")}>8</button>
+        <button onClick={() => setOutput(output + "9")}>9</button>
+        <button onClick={() => setOutput(eval(output) + "÷")}>÷</button>
+        <button onClick={() => setOutput(output + "4")}>4</button>
+        <button onClick={() => setOutput(output + "5")}>5</button>
+        <button onClick={() => setOutput(output + "6")}>6</button>
+        <button onClick={() => setOutput(eval(output) + "×")}>×</button>
+        <button onClick={() => setOutput(output + "1")}>1</button>
+        <button onClick={() => setOutput(output + "2")}>2</button>
+        <button onClick={() => setOutput(output + "3")}>3</button>
+        <button onClick={() => setOutput(eval(output) + "-")}>-</button>
+        <button onClick={() => setOutput(output + "0")}>0</button>
+        <button>.</button>
+        <button
+          onClick={() => {
+            try {
+              setOutput(eval(output));
+            } catch (e) {
+              setOutput("Error");
+              setTimeout(() => setOutput(""), 2000)
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          =
+        </button>
+        <button onClick={() => setOutput(eval(output) + "+")}>+</button>
+      </div>
     </div>
   );
 }
 
+const lock = document.createElement("meta");
+lock.name = "darkreader-lock";
+document.head.appendChild(lock);
 export default App;
